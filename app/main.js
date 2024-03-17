c5182e5382541c323433f4f1b79c42ec734df87a
Ionut <ionionut@gmail.com>
2024-03-17 11:03:43 +0200

    stage 3

--- app/main.js
+++ app/main.js
@@ -1,8 +1,22 @@
 const net = require("net");
+const path = require('path');
 
+// Utility function to get the timestamp
 const getTimestamp = () => {
   const date = new Date();
   return date.toISOString();
+}
+
+// Parse the request
+const parseRequest = (data) => {
+  const request = data.toString();
+  const lines = request.split("\r\n");
+  const firstLine = lines[0];
+  const [method, path, version] = firstLine.split(" ");
+  const headers = lines.slice(1, lines.length - 2);
+  const body = lines[lines.length - 1];
+  
+  return { method, path, version, headers, body };
 }
 
 const server = net.createServer((socket) => {
@@ -14,8 +28,20 @@ const server = net.createServer((socket) => {
 
   // Sending a message to the client
   socket.on("data", (data) => {
-    console.log("[", getTimestamp(), "] - Data received: ", data.toString());
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    console.log("[", getTimestamp(), "] - Data received: <<Start>>");
+    console.log(data.toString());
+    console.log("[", getTimestamp(), "] - Data received: <<End>>");
+
+    // Parse the request and create an object
+    const request = parseRequest(data);
+    console.log("[", getTimestamp(), "] - Request object:", request);
+
+    if (request.path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
   });
 });
 
