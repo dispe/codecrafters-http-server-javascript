cf8dc687cfda0b8f008ee2fe90bd4f693750c5dc
Ionut <ionionut@gmail.com>
2024-03-17 10:43:32 +0200

    stage 2

--- app/main.js
+++ app/main.js
@@ -1,9 +1,21 @@
 const net = require("net");
 
+const getTimestamp = () => {
+  const date = new Date();
+  return date.toISOString();
+}
+
 const server = net.createServer((socket) => {
+  // Closing the server when the client disconnects
   socket.on("close", () => {
     socket.end();
     server.close();
+  });
+
+  // Sending a message to the client
+  socket.on("data", (data) => {
+    console.log("[", getTimestamp(), "] - Data received: ", data.toString());
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
   });
 });
 
